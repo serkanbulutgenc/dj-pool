7c087a72583b311ea8b3acb3676a2a6cd1f14435
Serkan <serkanbulutgenc@gmail.com>
2024-08-05 08:23:34 +0000

    Created views function for polls application.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,32 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+#from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index")
+    latest_question_list= Question.objects.order_by("-pub_date")[:5]
+    #template = loader.get_template("polls/index.html")
+
+    context={"latest_question_list": latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    '''
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist as e:
+        raise Http404("Question does not exists")
+    '''
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', context={'question':question})
+
+def results(request, question_id):
+    response="You're looking at the results of question %s"
+
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
